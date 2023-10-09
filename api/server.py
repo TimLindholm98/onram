e3d9ec9fe72262a8fec814ee6e09623786674732
TimLindholm98 <timpanmili98@gmail.com>
2023-10-09 19:45:52 +0200

    First try

--- api/server.py
+++ api/server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 
 from flask import Flask, jsonify
 from flask_mysqldb import MySQL
@@ -19,13 +20,15 @@ def add_data():
     hostname = request.json['hostname']
     ip = request.json['ip']
     ipmi = request.json['ipmi']
+    cpu = request.json['cpu']
     ram = request.json['ram']
+    ram_sticks = request.json['ram_sticks']
     threads = request.json['threads']
     nvme = request.json['nvme']
     ssd = request.json['ssd']
     hdd = request.json['hdd']
     
-    cur.execute('''INSERT INTO test_suite (hostname, ip, ipmi, ram, threads, nvme, ssd, hdd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''', hostname, ip, ipmi, ram, threads, nvme, ssd, hdd
+    cur.execute('''INSERT INTO test_suite (hostname, ip, ipmi, cpu, threads, ram, ram_sticks, threads, nvme, ssd, hdd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', hostname, ip, ipmi, cpu, threads, ram, ram_sticks, nvme, ssd, hdd
 )
     mysql.connection.commit()
     cur.close()
@@ -33,7 +36,7 @@ def add_data():
 
 
 @app.route('/data/<string:hostname>', methods=['DELETE'])
-def delete_data(hostname):
+def delete_data_by_hostname(hostname):
     cur = mysql.connection.cursor()
     
     cur.execute('''DELETE FROM test_suite WHERE hostname = %s''', (hostname,))
@@ -42,13 +45,29 @@ def delete_data(hostname):
     return jsonify({'message': 'Data deleted successfully'})
 
 
+@app.route('/', methods=['GET'])
+def get_root():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM test_suite''')
+    data = cur.fetchall()
+    cur.close()
+    return json2html.convert(json = data)
+    
 @app.route('/data', methods=['GET'])
 def get_data():
     cur = mysql.connection.cursor()
     cur.execute('''SELECT * FROM test_suite''')
     data = cur.fetchall()
     cur.close()
-    return json2html.convert(json = data)
+    return jsonify(data)
+    
+@app.route('/data/<string:hostname>', methods=['GET'])
+def get_data_by_hostname(hostname):
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM test_suite WHERE hostname = %s''', (hostname,))
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ client/onram_client.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+api_endpoint=""
+api_port=""
+
+
+# Get short easy to look at values. 
+get_cpu_name(){
+    echo "$(lscpu --json | jq '.lscpu[] | select(.field=="Model name:") | .data' | tr -d '"')"
+}
+get_threads(){
+    echo "$(nproc)"
+}
+get_ram_total(){
+    echo "$(lsmem --json | jq .memory[].size | tr -d '"')"
+}
+get_ram_sticks(){
+    echo "$(dmidecode -t memory | grep -E "^\sSize\:.*\sGB$" | awk -F: '{print $NF}' | tr -d [:blank:] | uniq -c | awk '{print $1}')"
+}
+get_hostname(){
+    echo "$(hostname)"
+}
+get_ip_addr(){
+    echo "$(for i in $(hostname -I); do echo $i | awk -F. '/10.21.30/{print $0}' ; done)"
+}
+get_ipmi_addr(){
+    echo "$(ipmitool lan print | grep "IP Address  " | cut -d: -f2 | tr -d [:blank:])"
+}
+get_all_nvme(){
+    echo "$(lsblk -n -o name,size,rota,type /dev/nvme* 2> /dev/null | awk '$NF ~ /disk/{print $1 "," $2 "," $3}' |  awk -F, '$NF ~ /0/{print $1 "," $2}' | wc -l)"
+}
+get_all_ssd(){
+    echo "$(lsblk -n -o name,size,rota,type /dev/sd* 2> /dev/null | awk '$NF ~ /disk/{print $1 "," $2 "," $3}' |  awk -F, '$NF ~ /0/{print $1 "," $2}' | wc -l)"
+}
+get_all_hdd(){
+    echo "$(lsblk -n -o name,size,rota,type /dev/sd* 2> /dev/null | awk '$NF ~ /disk/{print $1 "," $2 "," $3}' |  awk -F, '$NF ~ /1/{print $1 "," $2}' | wc -l)"
+}
+
+
+# More information
+list_ram_sticks(){
+    echo "$(dmidecode -t memory | grep -E "^\sSize\:.*\sGB$" | awk -F: '{print $NF}' | tr -d [:blank:] | uniq -c | awk '{print $1 "," $2}')"
+}
+list_all_nvme(){
+    echo "$(lsblk -n -o name,size,rota,type /dev/nvme* 2> /dev/null | awk '$NF ~ /disk/{print $1 "," $2 "," $3}' |  awk -F, '$NF ~ /0/{print $1 "," $2}')"
+}
+list_all_ssd(){
+    echo "$(lsblk -n -o name,size,rota,type /dev/sd* 2> /dev/null | awk '$NF ~ /disk/{print $1 "," $2 "," $3}' |  awk -F, '$NF ~ /0/{print $1 "," $2}')"
+}
+list_all_hdd(){
+    echo "$(lsblk -n -o name,size,rota,type /dev/sd* 2> /dev/null | awk '$NF ~ /disk/{print $1 "," $2 "," $3}' |  awk -F, '$NF ~ /1/{print $1 "," $2}')"
+}
+
+# https://stackoverflow.com/questions/48470049/build-a-json-string-with-bash-variables
+
+# Generating a JSON string (https://stackoverflow.com/a/68591585)
+json_string=$(
+  jq --null-input \
+    --arg hostname "$(get_hostname)" \
+    --arg ip "$(get_ip_addr)" \
+    --arg ipmi "$(get_ipmi_addr)" \
+    --arg cpu "$(get_cpu_name)" \
+    --arg threads "$(get_threads)" \
+    --arg ram "$(get_ram_total)" \
+    --arg ram_sticks "$(get_ram_sticks)" \
+    --arg nvme "$(get_all_nvme)" \
+    --arg ssd "$(get_all_ssd)" \
+    --arg hdd "$(get_all_hdd)" \
+    '$ARGS.named'
+)
+
+printf "%s" "$json_string" > /tmp/onram_client.json
+
+# curl -X POST -H "Content-Type: application/json" -d @FILENAME DESTINATION
+
+curl --header "Content-Type: application/json" \
+  --request "POST" \
+  --data "@/tmp/onram_client.json" \
+  http://localhost:3000/api/login
