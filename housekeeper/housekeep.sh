c5be1231cdce14333d4d3b0349fc72190c611e74
TimLindholm98 <timpanmili98@gmail.com>
2024-01-02 15:06:32 +0100

    Adding removal code for housekeep.sh

--- housekeeper/housekeep.sh
+++ housekeeper/housekeep.sh
@@ -24,6 +24,22 @@ stop_client(){
     http://${api_endpoint}:${api_port}/data/power_state/${hostname}
 }
 
+finish_client(){
+    local hostname="$1"
+    json_string=$(
+    jq --null-input \
+        --arg power_state "finished" \
+        --arg date_time "$(get_date_time)" \
+        '$ARGS.named'
+    )
+    printf "%s" "$json_string" > /tmp/onram_client.json
+
+    curl --header "Content-Type: application/json" \
+    --request "PUT" \
+    --data "@/tmp/onram_client.json" \
+    http://${api_endpoint}:${api_port}/data/power_state/${hostname}
+}
+
 get_hosts(){
     if [[ $1 == "all" ]]; then
         echo $(curl -s http://onram.enginit.se:5000/data | jq . )
@@ -31,31 +47,36 @@ get_hosts(){
         echo $(curl -s http://onram.enginit.se:5000/data | jq '.[] | select(.power_state=="up")')
     elif [[ $1 == "down" ]]; then
         echo $(curl -s http://onram.enginit.se:5000/data | jq '.[] | select(.power_state=="down")')
-    elif [[ $1 == "done" ]]; then
-        echo $(curl -s http://onram.enginit.se:5000/data | jq '.[] | select(.power_state=="done")')
-    fi
-}
-
-down_host(){
-    # ping hostname from $1
-    # if down stop_client function
-    local host="$1"
-    ping -q -w 1 ${host} &> /dev/null ; exit_code=$?
-
-    if [[ $exit_code != 0 ]]; then
-        stop_client ${host}
-        echo "${host} is now down"
+    elif [[ $1 == "housekeep" ]]; then
+        echo $(curl -s http://onram.enginit.se:5000/data/housekeeping | jq '.[]' )
+    elif [[ $1 == "finished" ]]; then
+        echo $(curl -s http://onram.enginit.se:5000/data | jq '.[] | select(.power_state=="finished")')
     fi
 }
 
 down_hosts(){
-    for i in $(get_hosts up | jq .hostname | tr -d '"'); do
-        down_host $i
+    for host in $(get_hosts up | jq .hostname | tr -d '"'); do
+        ping -q -w 1 ${host} &> /dev/null ; exit_code=$?
+
+        if [[ $exit_code != 0 ]]; then
+            stop_client ${host}
+            echo "${host} is now down"
+        fi
     done
 }
 
 
+finish_hosts(){
+    for i in $(get_hosts housekeep | jq .hostname | tr -d '"'); do
+        finish_client $i
+        echo "${host} is now finished"
+    done
+}
+
 # Starting clean up
 echo "Housekeeping: started $(get_date_time)"
+echo  "Cleaning up hosts that are set to up" 
 down_hosts
+echo "Finishing old hosts"
+finish_hosts
 echo "Housekeeping: ended $(get_date_time)"
